31639107afb02990601df4401d082d606208f514
Sheikh-Antor <73622061+Sheikh-Antor@users.noreply.github.com>
2021-01-06 17:29:31 +0600

    Create main.js

---
+++ main.js
@@ -0,0 +1,17 @@
+let targetImg=document.querySelector('.starbucks');
+let img1=document.getElementById('img1');
+let img2=document.getElementById('img2');
+let img3=document.getElementById('img3');
+ 
+img1.addEventListener('click',function(){
+     targetImg.src="https://www.freepngimg.com/thumb/starbucks/77325-frappuccino-drink-chocolate-starbucks-matcha-white.png";
+})
+
+img2.addEventListener('click',function(){
+    targetImg.src="https://freepngimg.com/thumb/coffee/62115-tea-coffee-drink-starbucks-latte-free-download-image.png";
+})
+
+img3.addEventListener('click',function(){
+    targetImg.src="https://freepngimg.com/thumb/coffee/62120-coffee-frappuccino-drink-latte-starbucks-unicorn.png";
+})
+ 
